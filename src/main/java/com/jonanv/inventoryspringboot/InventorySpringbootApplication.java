2c9dc7c5129b75fe3f8530098a9d3547059413fa
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-02-08 22:19:31 -0500

    :tada: first commit

---
+++ src/main/java/com/jonanv/inventoryspringboot/InventorySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.jonanv.inventoryspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorySpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonanv/inventoryspringboot/InventorySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonanv.inventoryspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
