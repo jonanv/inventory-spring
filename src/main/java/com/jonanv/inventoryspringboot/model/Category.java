16665d14a108253dcb9b1befbab4706f08fa7449
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-02-13 22:24:52 -0500

    :truck: move class category

--- src/main/java/com/jonanv/inventoryspringboot/model/Category.java
+++ src/main/java/com/jonanv/inventoryspringboot/model/Category.java
@@ -1,16 +1,23 @@
-package model;
+package com.jonanv.inventoryspringboot.model;
 
 import java.io.Serializable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name="category")
 public class Category implements Serializable{
 
 	private static final long serialVersionUID = -8584834441521355363L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String description;
