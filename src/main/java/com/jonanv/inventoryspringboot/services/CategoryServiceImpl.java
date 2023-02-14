295982885b41eada6b4ec23ce2de75a28bb38c30
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-02-13 23:04:01 -0500

    :sparkles: add implemetation of service's category

---
+++ src/main/java/com/jonanv/inventoryspringboot/services/CategoryServiceImpl.java
@@ -0,0 +1,17 @@
+package com.jonanv.inventoryspringboot.services;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.jonanv.inventoryspringboot.response.CategoryResponseRest;
+
+@Service
+public class CategoryServiceImpl implements ICategoryService {
+
+	@Override
+	public ResponseEntity<CategoryResponseRest> search() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
