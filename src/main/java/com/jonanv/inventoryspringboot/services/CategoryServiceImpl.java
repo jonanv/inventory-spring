f2c7f96f969169b7808dab04f5d787bc6e61b7ed
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-02-15 23:12:14 -0500

    :sparkles: add implementacion of category service

--- src/main/java/com/jonanv/inventoryspringboot/services/CategoryServiceImpl.java
+++ src/main/java/com/jonanv/inventoryspringboot/services/CategoryServiceImpl.java
@@ -1,17 +1,39 @@
 package com.jonanv.inventoryspringboot.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.jonanv.inventoryspringboot.dao.ICategoryDao;
+import com.jonanv.inventoryspringboot.model.Category;
 import com.jonanv.inventoryspringboot.response.CategoryResponseRest;
 
 @Service
 public class CategoryServiceImpl implements ICategoryService {
+	
+	@Autowired
+	private ICategoryDao categoryDao;
 
 	@Override
+	@Transactional(readOnly = true)
 	public ResponseEntity<CategoryResponseRest> search() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		CategoryResponseRest response = new CategoryResponseRest();
+		
+		try {
+			List<Category> category =  (List<Category>) categoryDao.findAll();
+			response.getCategoryResponse().setCategory(category);
+			response.setMetadata("Respuesta ok", "00", "Respuesta exitosa");
+		} catch (Exception e) {
+			response.setMetadata("Respuesta nok", "-1", "Error al consultar");
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
 	}
 
 }
