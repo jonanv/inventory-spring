6793d172a2c2cb227cac0ffff29a4dec1473eb9c
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-02-17 20:48:44 -0500

    :sparkles: add method save to service

--- src/main/java/com/jonanv/inventoryspringboot/services/ICategoryService.java
+++ src/main/java/com/jonanv/inventoryspringboot/services/ICategoryService.java
@@ -2,11 +2,13 @@ package com.jonanv.inventoryspringboot.services;
 
 import org.springframework.http.ResponseEntity;
 
+import com.jonanv.inventoryspringboot.model.Category;
 import com.jonanv.inventoryspringboot.response.CategoryResponseRest;
 
 public interface ICategoryService {
 	
 	public ResponseEntity<CategoryResponseRest> search();
 	public ResponseEntity<CategoryResponseRest> searchById(Long id);
+	public ResponseEntity<CategoryResponseRest> save(Category category);
 
 }
