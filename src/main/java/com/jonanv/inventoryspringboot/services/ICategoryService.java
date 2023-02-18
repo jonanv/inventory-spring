01a77294a0390e31f8d113be42fe8bf5acc84a57
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-02-17 20:12:31 -0500

    :sparkles: add method search category by id

--- src/main/java/com/jonanv/inventoryspringboot/services/ICategoryService.java
+++ src/main/java/com/jonanv/inventoryspringboot/services/ICategoryService.java
@@ -7,5 +7,6 @@ import com.jonanv.inventoryspringboot.response.CategoryResponseRest;
 public interface ICategoryService {
 	
 	public ResponseEntity<CategoryResponseRest> search();
+	public ResponseEntity<CategoryResponseRest> searchById(Long id);
 
 }
