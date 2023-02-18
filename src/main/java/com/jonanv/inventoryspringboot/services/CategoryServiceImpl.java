59176af99287cae2ae0adae0d7ff72d32b9ed409
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-02-17 20:13:19 -0500

    :sparkles: add implement of method search category by id

--- src/main/java/com/jonanv/inventoryspringboot/services/CategoryServiceImpl.java
+++ src/main/java/com/jonanv/inventoryspringboot/services/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.jonanv.inventoryspringboot.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -30,6 +32,32 @@ public class CategoryServiceImpl implements ICategoryService {
 			response.setMetadata("Respuesta ok", "00", "Respuesta exitosa");
 		} catch (Exception e) {
 			response.setMetadata("Respuesta nok", "-1", "Error al consultar");
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public ResponseEntity<CategoryResponseRest> searchById(Long id) {
+		
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		
+		try {
+			Optional<Category> category = categoryDao.findById(id);
+			
+			if (category.isPresent()) {
+				response.getCategoryResponse().setCategory(list);
+				response.setMetadata("Respuesta nok", "00", "Categoria encontrada");
+			} else {
+				response.setMetadata("Respuesta nok", "-1", "Categoria no encontrada");
+				return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+			
+		} catch (Exception e) {
+			response.setMetadata("Respuesta nok", "-1", "Error al consultar por id");
 			e.getStackTrace();
 			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
