8cc412b484c7a8a1673556e910fe0c2e884c18eb
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-02-17 20:14:13 -0500

    :sparkles: add controller search category by id

--- src/main/java/com/jonanv/inventoryspringboot/controller/CategoryRestController.java
+++ src/main/java/com/jonanv/inventoryspringboot/controller/CategoryRestController.java
@@ -3,6 +3,7 @@ package com.jonanv.inventoryspringboot.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,10 +17,28 @@ public class CategoryRestController {
 	@Autowired
 	private ICategoryService service;
 	
+	
+	/**
+	 * get all the categories
+	 * @return
+	 */
 	@GetMapping("/categories")
 	public ResponseEntity<CategoryResponseRest> searchCategory() {
 		
 		ResponseEntity<CategoryResponseRest> response = service.search();
+		return response;
+		
+	}
+	
+	/**
+	 * get categories by id
+	 * @param id
+	 * @return
+	 */
+	@GetMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> searchCategoryById(@PathVariable Long id) {
+		
+		ResponseEntity<CategoryResponseRest> response = service.searchById(id);
 		return response;
 		
 	}
