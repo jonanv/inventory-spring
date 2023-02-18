72f643c0c968d7621bbcd9af076a1366322b6740
Johanny Vargas González <jonan-vargas23@hotmail.com>
2023-02-17 20:49:18 -0500

    :sparkles: implement method save categories

--- src/main/java/com/jonanv/inventoryspringboot/services/CategoryServiceImpl.java
+++ src/main/java/com/jonanv/inventoryspringboot/services/CategoryServiceImpl.java
@@ -64,4 +64,31 @@ public class CategoryServiceImpl implements ICategoryService {
 		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
 	}
 
+	@Override
+	@Transactional(readOnly = true)
+	public ResponseEntity<CategoryResponseRest> save(Category category) {
+		
+		CategoryResponseRest response = new CategoryResponseRest();
+		List<Category> list = new ArrayList<>();
+		
+		try {
+			Category categorySave = categoryDao.save(category);
+			
+			if (categorySave != null) {
+				list.add(categorySave);
+				response.getCategoryResponse().setCategory(list);
+				response.setMetadata("Respuesta ok", "00", "Categoria guardada");
+			} else {
+				response.setMetadata("Respuesta nok", "-1", "Categoria no guardada");
+				return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.BAD_REQUEST);
+			}
+			
+		} catch (Exception e) {
+			response.setMetadata("Respuesta nok", "-1", "Error al guardar categoria");
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity<CategoryResponseRest>(response, HttpStatus.OK);
+	}
+
 }
